a394cf9896dcb4c43e5b843a6041e42497fd7c79
Mohamed3li454 <mohhamed4413@gmail.com>
2024-11-16 17:49:13 +0200

    finally

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 void main() {
   runApp(const MyApp());
 }
-//gg
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
